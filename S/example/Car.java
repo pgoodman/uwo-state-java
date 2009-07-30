659d501fc1cc4882076ba24a198eb7c9ee16cda7
peter.goodman <peter.goodman@54eaba10-7ca8-11de-87c4-3d7c82793bd4>
2009-07-30 01:55:05 +0000

    Adding in compiled example folder.

---
+++ S/example/Car.java
@@ -0,0 +1,32 @@
+package example;
+import example.Auto;
+import example.SM;
+public class Car extends Auto {
+	public Car() {
+		this.__cs = 3;
+		try {
+ 
+        System.out.println("CAR: Off (constructor)"); 
+    
+		} catch(Exception e) {
+			System.out.println(e);
+			System.exit(1);
+		}
+	}
+	public void reverse() {
+		this.__ps = this.__cs;
+		if((this.__cs = SM.trans[this.__cs][2]) < 0) {
+			this.__cs = this.__ps;
+			super.reverse();
+			return;
+		}
+		try {
+ 
+        System.out.println("CAR: Reverse (from Forward)."); 
+    
+		} catch(Exception e) {
+			System.out.println(e);
+			System.exit(1);
+		}
+	}
+}
--- example/Auto.java
+++ example/Auto.java
@@ -4,23 +4,42 @@ package example;
 public class Auto {
     states { :Forward, :Neutral, :Reverse, :Off }
     
-    public Auto() :Off { System.out.println("AUTO: Off (constructor)"); }
+    public Auto() :Off { 
+        System.out.println("AUTO: Off (constructor)"); 
+    }
     
-    /* the turnOff method shows a motivation for having the ability to
-       remember a previous state. */
-    public void turnOn() :Off -> :Neutral { System.out.println("AUTO: On (Neutral)."); }
+    /* the turnOff method shows a motivation for having the 
+       ability to remember a previous state. */
+      
+    public void turnOn() :Off -> :Neutral { 
+        System.out.println("AUTO: On (Neutral)."); 
+    }
     
-    /* the following methods ass a motivation for having the ability to
-       perform set operations, such as difference, on states. */
+    /* the following methods ass a motivation for having 
+       the ability to perform set operations, such as 
+       difference, on states. */
     
-    public void turnOff() :Forward, :Neutral, :Reverse -> :Off { System.out.println("AUTO: Off."); }
+    public void turnOff() :Forward, :Neutral, :Reverse -> :Off { 
+        System.out.println("AUTO: Off."); 
+    }
     
-    /* this will be specialized by Car::reverse() */
-    public void reverse() :Forward, :Neutral -> :Reverse { System.out.println("AUTO: Reverse."); }
+    /* this will be specialized by Car::reverse() for the 
+       transition :Forward -> :Reverse */
+    public void reverse() :Forward, :Neutral -> :Reverse { 
+        System.out.println("AUTO: Reverse."); 
+    }
     
-    public void neutral() :Forward, :Reverse -> :Neutral { System.out.println("AUTO: Neutral."); }
+    public void neutral() :Forward, :Reverse -> :Neutral { 
+        System.out.println("AUTO: Neutral."); 
+    }
     
-    /* example of method merging */
-    public void forward() :Neutral -> :Forward { System.out.println("AUTO: Forward (from Neutral)."); }
-    public void forward() :Reverse -> :Forward { System.out.println("AUTO: Forward (from Reverse)."); }
+    /* the following two methods will be merged into one in the
+       compiled version. */
+    
+    public void forward() :Neutral -> :Forward { 
+        System.out.println("AUTO: Forward (from Neutral)."); 
+    }
+    public void forward() :Reverse -> :Forward { 
+        System.out.println("AUTO: Forward (from Reverse)."); 
+    }
 }
